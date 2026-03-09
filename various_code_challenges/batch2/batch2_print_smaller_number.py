1a98fb4a71d9f2eb954c929d36b36fee300787e0
nezjuan <juanmartinezat9@gmail.com>
2026-03-09 09:28:09 +0800

    finished batch2 program1 and moved files to a folder for easy compiling

---
+++ various_code_challenges/batch2/batch2_print_smaller_number.py
@@ -0,0 +1,6 @@
+number1=int(input("Enter number: "))
+number2=int(input("Enter number: "))
+if number1>number2:
+    print(number2,"is the smaller number")
+else:
+    print(number1,"is the smaller number")
