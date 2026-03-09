8065c1a4173ba28adcf06f213bda1f9b44e4f1ed
nezjuan <juanmartinezat9@gmail.com>
2026-03-09 10:57:52 +0800

    tweaked batch2 program10

--- various_code_challenges/batch2/batch2_numbers_between_two_given_numbers.py
+++ various_code_challenges/batch2/batch2_numbers_between_two_given_numbers.py
@@ -4,4 +4,4 @@ in_between_no=[]
 while no1<no2-1:
     no1+=1
     in_between_no.append(no1)
-print("The numbers between",no1-len(in_between_no),"and",no2,"are:",in_between_no)
+print("The numbers between",no1-len(in_between_no),"to",no2,"are:",in_between_no)
