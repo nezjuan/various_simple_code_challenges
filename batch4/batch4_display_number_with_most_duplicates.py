5c8a680b9236c535fb4e61ecf3c00ed648903a91
nezjuan <juanmartinezat9@gmail.com>
2026-03-20 11:53:29 +0800

    finished batch4 program2

---
+++ batch4/batch4_display_number_with_most_duplicates.py
@@ -0,0 +1,21 @@
+stored_numbers=[]
+duped_numbers=[]
+while True:
+    try:
+        number=int(input("Enter a Number: "))
+        if number not in stored_numbers:
+            stored_numbers.append(number)
+        elif number in stored_numbers:
+            duped_numbers.append(number)
+    except ValueError:
+        print("Invalid input")
+        break
+    print("The duplicated numbers are:",duped_numbers)
+    print("The current numbers are: ",stored_numbers)
+    if duped_numbers:
+        most_dup = max(duped_numbers, key=duped_numbers.count)
+        print(f"The number with the most duplicates is: {most_dup} (appeared {duped_numbers.count(most_dup)} times)")
+    else:
+        print("No duplicates found.")
+
+#Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate
