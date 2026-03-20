39a37dadc7ed072d79f3dc9d7a6ace3e0aeee677
nezjuan <juanmartinezat9@gmail.com>
2026-03-20 14:59:50 +0800

    accomplished batch7 program10

---
+++ batch7/batch7_r_index_function.py
@@ -0,0 +1,11 @@
+word=input("Enter a word/s: ")
+search_item=input("Enter the character or substring to find: ")
+found_index=-1
+for i in range(len(word)-len(search_item), -1, -1):
+    if word[i:i+len(search_item)]==search_item:
+        found_index = i
+        break
+if found_index != -1:
+    print("Last occurrence at index:",found_index)
+else:
+    print("ValueError: substring not found")
